3c9d43ee0aaad7381f7ab66a414fe4ffc659cd61
Danielnorbert22 <Daniel4cod1ng@gmail.com>
2025-07-11 14:14:28 +0100

    add skill endpoint#2

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './users/users.module';
-import { ChatModule } from './chat/chat.module'
+
 
 
 
